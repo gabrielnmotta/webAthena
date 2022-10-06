6605edfc173d415a46ce89154e2cc733528657a8
Gabriel Nogueira <gabrieln500@gmail.com>
2022-10-06 11:52:28 -0300

    some changes on design of the page

--- src/components/NavBar/Nav.tsx
+++ src/components/NavBar/Nav.tsx
@@ -1,25 +1,25 @@
 import "./style.scss";
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import SettingsIcon from '@mui/icons-material/Settings';
-import logo from "../../assets/Logo/ZeusAgro.jpeg";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import SettingsIcon from "@mui/icons-material/Settings";
 
-
-function Navbar() {
-    return (
-        <div className="navbar">
-            <img src={logo} className="icon" ></img>
-            <input className="busca" placeholder="buscar"></input>
-            <nav>
-                <ul className="menu">
-                    <li><NotificationsIcon/></li>
-                    <li><SettingsIcon/></li>
-                </ul>
-            </nav>
-
+export function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input className="busca" placeholder="buscar"></input>
         </div>
 
-    );
-
+        <div className="items">
+          <div className="item">
+            <NotificationsIcon className="icon"/>
+          </div>
+          <div className="item">
+            <SettingsIcon className="icon"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,56 +1,62 @@
-import { Divider } from "@mui/material";
 import BarChartIcon from "@mui/icons-material/BarChart";
 import MapIcon from "@mui/icons-material/Map";
-import SettingsIcon from '@mui/icons-material/Settings';
+import SettingsIcon from "@mui/icons-material/Settings";
 import LogoutIcon from "@mui/icons-material/Logout";
 import GroupIcon from "@mui/icons-material/Group";
 import "./sidebar.scss";
 import DashboardIcon from "@mui/icons-material/Dashboard";
+import Logo from '../../assets/Logo/ZeusAgro.jpeg'
 
 export function Sidebar() {
   return (
-    <div className="sidebar">
-      <div className="center">
-        <ul>
-          <div className="main">
-            <li>
-              <DashboardIcon className="icon" />
-              <span role="button">Dashboard</span>
-            </li>
-
-            <li>
-              <BarChartIcon className="icon" />
-              <span role="button">Statistics</span>
-            </li>
-
-            <li>
-              <MapIcon className="icon" />
-              <span role="button">Maps</span>
-            </li>
+    
+        <div className="sidebar">
+          <div className="top">
+                <img src={Logo} alt="logo zeus" className="logo"/>
           </div>
-
-          <Divider />
-
-          <div className="users">
-            <li>
-              <GroupIcon className="icon" />
-              <span role="button">Users</span>
-            </li>
+          
+          <div className="center">
+            <ul>
+              <div className="main">
+                <li>
+                  <DashboardIcon className="icon" />
+                  <span role="button">Dashboard</span>
+                </li>
+  
+                <li>
+                  <BarChartIcon className="icon" />
+                  <span role="button">Statistics</span>
+                </li>
+  
+                <li>
+                  <MapIcon className="icon" />
+                  <span role="button">Maps</span>
+                </li>
+              </div>
+  
+              
+  
+              <div className="users">
+                <li>
+                  <GroupIcon className="icon" />
+                  <span role="button">Users</span>
+                </li>
+              </div>
+  
+              
+              <div className="settings">
+                <li>
+                  <SettingsIcon className="icon" />
+                  <span role="button">Settings</span>
+                </li>
+                <li>
+                  <LogoutIcon className="icon" />
+                  <span role="button">Logout</span>
+                </li>
+              </div>
+            </ul>
           </div>
-
-          <Divider />
-          <div className="settings">
-            <li>
-              <SettingsIcon className="icon"/>
-              <span role="button">Settings</span>
-            </li>
-            <li>
-              <LogoutIcon className="icon" />
-              <span role="button">Logout</span>
-            </li>
-          </div>
-        </ul>
-      </div>
-    </div>
+        </div>
+     
   );
 }
